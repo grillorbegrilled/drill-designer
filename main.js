c51a9eedb56729fe0a6d7b67553121ddbea90cf6
grillorbegrilled <foutzjesse@gmail.com>
2025-08-07 20:11:50 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 const canvas = document.getElementById("fieldCanvas");
 const ctx = canvas.getContext("2d");
 
+// Default to 8-to-5 step size
+const stepSizeInches = 22.5;
+
 function render() {
-    const stepSizeInches = parseFloat(document.getElementById("stepSize").value);
     const { scaleX, scaleY } = drawField(ctx, stepSizeInches);
     drawKids(ctx, kids, scaleX, scaleY);
 }
-
-// Re-render when the step size changes
-//document.getElementById("stepSize").addEventListener("change", render);
 
 // Initial draw
 render();
