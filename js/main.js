786c231188ece4cd274a58adc9a7e8db1781fbee
grillorbegrilled <foutzjesse@gmail.com>
2025-08-08 16:08:43 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 // main.js
-import { togglePlay, rewind, stepForward, stepBackward, scrubToStep } from './controls.js';
-import { render } from './render.js';
-
 window.onload = () => {
+    ctx = document.getElementById("fieldCanvas").getContext("2d");
+
     document.getElementById("playBtn").addEventListener("click", togglePlay);
     document.getElementById("rewindBtn").addEventListener("click", rewind);
     document.getElementById("stepBackBtn").addEventListener("click", stepBackward);
     document.getElementById("stepForwardBtn").addEventListener("click", stepForward);
-    document.getElementById("scrubSlider").addEventListener("input", scrub
+    document.getElementById("scrubSlider").addEventListener("input", scrubToStep);
+
+    render();
+};
