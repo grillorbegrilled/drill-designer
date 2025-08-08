15dec6557410430a0da44e3cf9d2afae9dd7489c
grillorbegrilled <foutzjesse@gmail.com>
2025-08-08 15:58:13 -0400

    Create animation.js

---
+++ js/animation.js
@@ -0,0 +1,58 @@
+// animation.js
+import { kids, currentStep, snapshots, startingFormation } from './state.js';
+import { render } from './render.js';
+
+export function cloneKidStates() {
+    return kids.map(kid => ({ ...kid }));
+}
+
+export function applySnapshot(state) {
+    kids.forEach(kid => {
+        const match = state.find(s => s.id === kid.id);
+        if (match) {
+            Object.assign(kid, match);
+        }
+    });
+}
+
+export function advance(silent = false) {
+    kids.forEach(kid => {
+        const change = kid.changes.find(c => c.step === currentStep);
+        if (change) {
+            if (change.direction !== undefined) kid.direction = change.direction;
+            if (change.stop) kid.moving = false;
+        }
+
+        if (kid.moving) {
+            const radians = (kid.direction * Math.PI) / 180;
+            kid.x += Math.cos(radians);
+            kid.y += Math.sin(radians);
+        }
+    });
+
+    currentStep++;
+    if (currentStep % 16 === 0) {
+        snapshots.set(currentStep, cloneKidStates());
+    }
+
+    if (!silent) render();
+}
+
+export function simulateToStep(targetStep) {
+    let nearestSnapshotStep = 0;
+    for (let s of Array.from(snapshots.keys()).sort((a, b) => b - a)) {
+        if (s <= targetStep) {
+            nearestSnapshotStep = s;
+            break;
+        }
+    }
+
+    applySnapshot(snapshots.get(nearestSnapshotStep));
+    currentStep = nearestSnapshotStep;
+
+    for (let step = nearestSnapshotStep; step < targetStep; step++) {
+        advance(true);
+    }
+
+    render();
+}
