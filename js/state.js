f0ae48244b810074596e17a1ee3272684649cbe6
grillorbegrilled <foutzjesse@gmail.com>
2025-08-08 22:57:04 -0400

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,4 +1,4 @@
-// state.js
+// js/state.js
 var stepSizeInches = 22.5; // 8-to-5 default
 var currentStep = 0;
 var isPlaying = false;
@@ -22,14 +22,16 @@ var kids = [
     }
 ];
 
-var startingFormation = kids.map(kid => ({
-    id: kid.id,
-    x: kid.x,
-    y: kid.y,
-    direction: kid.direction,
-    moving: kid.moving,
-    color: kid.color
-}));
+var startingFormation = kids.map(function (kid) {
+    return {
+        id: kid.id,
+        x: kid.x,
+        y: kid.y,
+        direction: kid.direction,
+        moving: kid.moving,
+        color: kid.color
+    };
+});
 
 var snapshots = new Map();
 snapshots.set(0, JSON.parse(JSON.stringify(startingFormation)));
