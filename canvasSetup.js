85a8caf6b6831b8c0b66df059694ad1c133dbe9c
grillorbegrilled <foutzjesse@gmail.com>
2025-08-22 21:06:05 -0400

    Update canvasSetup.js

--- canvasSetup.js
+++ canvasSetup.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById("fieldCanvas");
 const ctx = canvas.getContext("2d");
 const stepSizeInches = 22.5; // 8-to-5 default
-const fieldWidthSteps = (160 * 12) / stepSizeInches;  // 85.33 for 8-to-5
+const fieldWidthSteps = 84; //round it down. //(160 * 12) / stepSizeInches;  // 85.33 for 8-to-5
 const fieldLengthSteps = (300 * 12) / stepSizeInches; // 160 for 8-to-5
 const scaleX = ctx.canvas.width / fieldLengthSteps;
 const scaleY = ctx.canvas.height / fieldWidthSteps;
