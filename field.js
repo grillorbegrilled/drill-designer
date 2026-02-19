7ec6d65038c6dbe80fa8d325484ab2ec7b377316
grillorbegrilled <foutzjesse@gmail.com>
2026-02-19 13:23:52 -0500

    Update field.js

--- field.js
+++ field.js
@@ -1,7 +1,7 @@
-function drawField(ctx, forPrint = false) {  
+function drawField(ctx) {  
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     //green
-    ctx.fillStyle = forPrint ? "white" : "#0b6623";
+    ctx.fillStyle = printMode ? "white" : "#0b6623";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
   
 // Draw step grid (light gray)==========
@@ -19,7 +19,7 @@ for (let x = 0; x <= fieldLengthSteps; x++) {
     } else {
         ctx.lineWidth = 0.5; // normal
     }
-    ctx.strokeStyle = forPrint ? "black" : "white";
+    ctx.strokeStyle = printMode ? "black" : "white";
     ctx.stroke();
 }
 
@@ -34,14 +34,14 @@ for (let y = 0; y <= fieldWidthSteps; y++) {
     } else {
         ctx.lineWidth = 0.5; // normal
     }
-    ctx.strokeStyle = forPrint ? "black" : "white";
+    ctx.strokeStyle = printMode ? "black" : "white";
     ctx.stroke();
 }
 
 //add yard lines and numbers============
 ctx.lineWidth = 3;
-ctx.strokeStyle = forPrint ? "black" : "white";
-ctx.fillStyle = forPrint ? "lightgray" : "white";
+ctx.strokeStyle = printMode ? "black" : "white";
+ctx.fillStyle = printMode ? "lightgray" : "white";
 ctx.font = `${scaleY * (72 / stepSizeInches) * 1.5}px sans-serif`; // 2 yards tall
 ctx.textAlign = "center";
 ctx.textBaseline = "middle";
@@ -88,7 +88,7 @@ for (let x = 0; x <= fieldLengthSteps; x += 8) {
     let topHashY = hashFromSidelineSteps * scaleY;
     let bottomHashY = ctx.canvas.height - topHashY;
 
-    ctx.strokeStyle = forPrint ? "black" : "white";
+    ctx.strokeStyle = printMode ? "black" : "white";
     ctx.lineWidth = 3;
 
     for (let x = 0; x <= fieldLengthSteps; x += 8) {
